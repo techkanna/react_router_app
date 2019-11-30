86ad7a729daf520ede66ace30ad7b135a9019d56
Senthil Kannan <senthilkannanarmy@gmail.com>
2019-11-30 14:29:04 +0530

    Updates

---
+++ precache-manifest.f177cc62234a9da0b82346a402f0c989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff09990369bf0b3516385ca902a37776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "212cd90155d274dcdade",
+    "url": "/static/css/main.0c968f4e.chunk.css"
+  },
+  {
+    "revision": "6b770e034918a252f097",
+    "url": "/static/js/2.e230d8bb.chunk.js"
+  },
+  {
+    "revision": "212cd90155d274dcdade",
+    "url": "/static/js/main.838a954a.chunk.js"
+  },
+  {
+    "revision": "f9d061cddadd2da0dc9a",
+    "url": "/static/js/runtime-main.7bfd02b7.js"
+  }
+]);
